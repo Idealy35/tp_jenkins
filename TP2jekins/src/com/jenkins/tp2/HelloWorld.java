f6421e35f2dd0dd607d72f2e5dcbd2c841dc2fbd
Raherinasandratra Idealy <raherinasandratraidealy@gmail.com>
2026-06-07 17:52:15 +0300

    Deuxième commit HelloWorld

--- TP2jekins/src/com/jenkins/tp2/HelloWorld.java
+++ TP2jekins/src/com/jenkins/tp2/HelloWorld.java
@@ -1,9 +1,7 @@
 package com.jenkins.tp2;
 
 public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, world");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Bonjour Jenkins !");
+    }
 }
