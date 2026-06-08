e1e5c3b53f476844ca6dbca3c819a5fe22f87cbe
TonNom <TonEmail>
2026-06-08 18:27:17 +0300

    Test webhook Jenkins

--- TP2jekins/src/com/jenkins/tp2/HelloWorld.java
+++ TP2jekins/src/com/jenkins/tp2/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jenkins.tp2;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Test webhook Jenkins ");
+        System.out.println("Test webhook Jenkins !");
     }
 }
